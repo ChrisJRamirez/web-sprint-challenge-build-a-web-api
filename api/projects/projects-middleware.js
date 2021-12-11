f6f13ae608142ecbd081344a3dc3c390a68a9589
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-12-10 20:25:08 -0500

    logger middleware functioning, working on other middlewares

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,19 @@
 // add middlewares here related to projects
+const Project = require("./projects-model");
+
+function logger(req, res, next) {
+  const date = new Date();
+  console.log(`
+    REQUEST METHOD: ${req.method},
+    REQUEST URL: ${req.originalUrl},
+    TIMESTAMP: ${date.toLocaleString()}
+  `)
+  next()
+};
+
+
+// export Middlewares
+module.exports = {
+  logger
+}
+
--- api/server.js
+++ api/server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const server = express();
 
 // Configure your server here
+server.use(express.json());
+
+// global middlewares and the user's router need to be connected here
+const projectRouter = require("./projects/projects-router");
+const {logger} = require("./projects/projects-middleware")
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
+server.use("/api/projects", logger, projectRouter);
 // Do NOT `server.listen()` inside this file!
+
+server.get("/", (req, res) => {
+  res.json("Server is up and running")
+})
 
 module.exports = server;
